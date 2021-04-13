a3c0a89540a40ac145e7f12d2323b876176912f3
Radu Nicolau <radu.nicolau@gaincredit.com>
2021-04-13 13:02:56 +0100

    Added three.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Display from './components/Display/Display';
+import './styles/App.css';
 
-ReactDOM.render(<App />, document.getElementById("main"));
+ReactDOM.render(<Display />, document.getElementById('main'));
