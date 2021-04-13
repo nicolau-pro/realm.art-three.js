b5901fce45de0c155e989ba456a07a3e8e1f7534
Radu Nicolau <radu.nicolau@gaincredit.com>
2021-04-13 22:48:10 +0100

    ready to  demo

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import Floor from './components/Models/Floor';
 import Mesh from './components/Animations/Mesh';
 import Whale from './components/Animations/Whale';
 
-const Scene = () => {
-  return (
-    <group>
-      <Floor />
-      <Suspense fallback={<Mesh />}>
-        <Whale />
-      </Suspense>
-      <Lights />
-    </group>
-  );
-};
-
 export default function Display() {
+  const Scene = () => {
+    return (
+      <group>
+        <Floor />
+        <Suspense fallback={<Mesh />}>
+          <Whale />
+        </Suspense>
+        <Lights />
+      </group>
+    );
+  };
+
   return (
     <Canvas style={{ background: 'midnightBlue' }} shadowMap colorManagement>
       <CameraController />
